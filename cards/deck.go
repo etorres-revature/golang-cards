bcf4d7dc61d45801953f6852d6bfa56c21ea74b8
Eric D. Torres <eric.d.torres@ibm.com>
2021-07-13 12:00:02 -0500

    creating a new deck of cards

---
+++ cards/deck.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+//create a new type of 'deck'
+//which is a slice of type string
+type deck []string
+
+func newDeck() deck {
+	cards := deck{}
+
+	cardSuits := []string{"Spades", "Hearts", "Clubs", "Diamonds"}
+	cardValues := []string{"Ace", "deuce", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "Jack", "Queen", "King"}
+
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+
+	return cards
+}
+
+func (d deck) print() {
+	for i, card := range d {
+		fmt.Println(i, card)
+	}
+}
