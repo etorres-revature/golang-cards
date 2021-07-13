40111f7609be810649fcbb32982128fc20a8b2cc
Eric D. Torres <eric.d.torres@ibm.com>
2021-07-13 16:18:25 -0500

    more fun with maps

--- maps/main.go
+++ maps/main.go
@@ -3,10 +3,19 @@ package main
 import "fmt"
 
 func main() {
-	colors := map[string]string{
-		"red":   "#ff0000",
-		"green": "#4bf745",
-	}
+	// var colors map[string]string
+
+	colors := make(map[string]string)
+
+	// colors := map[string]string{
+	// 	"red":   "#ff0000",
+	// 	"green": "#4bf745",
+	// }
+
+	colors["white"] = "#ffffff"
+	colors["red"] = "#ff0000"
+
+	delete(colors, "white")
 
 	fmt.Println(colors)
 }
