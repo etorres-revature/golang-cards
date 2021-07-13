7b0ab494841e61036e2179b3a944c9da55fd02af
Eric D. Torres <eric.d.torres@ibm.com>
2021-07-13 18:57:56 -0500

    adding new languages while learning about go interfaces

--- interfaces/main.go
+++ interfaces/main.go
@@ -7,13 +7,22 @@ type bot interface {
 }
 type englishBot struct{}
 type spanishBot struct{}
+type russianBot struct{}
+type frenchBot struct{}
+type italianBot struct{}
 
 func main() {
 	eb := englishBot{}
 	sb := spanishBot{}
+	rb := russianBot{}
+	fb := frenchBot{}
+	ib := italianBot{}
 
 	printGreeting(eb)
 	printGreeting(sb)
+	printGreeting(rb)
+	printGreeting(fb)
+	printGreeting(ib)
 }
 
 func printGreeting(b bot) {
@@ -28,4 +37,19 @@ func (eb englishBot) getGreeting() string {
 func (sb spanishBot) getGreeting() string {
 	//cutom logic for generating a greeting in the Spanish language
 	return "Hola, ese..."
+}
+
+func (rb russianBot) getGreeting() string {
+	//cutom logic for generating a greeting in the Russian language
+	return "Das vidanya..."
+}
+
+func (fb frenchBot) getGreeting() string {
+	//cutom logic for generating a greeting in the French language
+	return "Bonjour, mademoiselle..."
+}
+
+func (ib italianBot) getGreeting() string {
+	//cutom logic for generating a greeting in the Italian language
+	return "Ciao, bella..."
 }
