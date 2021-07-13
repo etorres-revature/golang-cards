2e72a3f1278624da2f51ece0e521a7b3cc5559a9
Eric D. Torres <eric.d.torres@ibm.com>
2021-07-13 14:23:26 -0500

    more tests for first and last card

--- cards/deck_test.go
+++ cards/deck_test.go
@@ -8,4 +8,12 @@ func TestNewDeck(t *testing.T) {
 	if len(d) != 52 {
 		t.Errorf("That's not enough cards! - Expected deck length of 52 cards, but got %v", len(d))
 	}
+
+	if d[0] != "Ace of Spades" {
+		t.Errorf("Expected first card in deck to be the Ace of Spaces, but got %v", d[0])
+	}
+
+	if d[len(d)-1] != "King of Diamonds" {
+		t.Errorf("Expected last card in deck to be the King of Clubs, but got %v", d[len(d)-1])
+	}
 }
