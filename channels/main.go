3e88117948e9e117ed5e7daf524af6585c493ba6
Eric D. Torres <eric.d.torres@ibm.com>
2021-07-13 21:25:34 -0500

    infinite loop through channel

--- channels/main.go
+++ channels/main.go
@@ -20,20 +20,19 @@ func main() {
 		go checkLink(link, c)
 	}
 
-	for i := 0; i < len(links); i++ {
-		fmt.Println(<-c)
+	for {
+		go checkLink(<-c, c)
 	}
-
 }
 
 func checkLink(link string, c chan string) {
 	_, err := http.Get(link)
 	if err != nil {
 		fmt.Println(link, "might be down...")
-		c <- "Might be down ... I think"
+		c <- link
 		return
 	}
 
 	fmt.Println(link, "is up and running...")
-	c <- "Alright; it is up!"
+	c <- link
 }
